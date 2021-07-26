6ad72af326c67c64b6bf23d684a5d3fe9fac9253
Leonardo-Antunes <leonardo.antunes@globalhitss.com.br>
2021-07-26 10:17:16 -0300

    added some styles to sidebar

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Container  = styled.div`
-    //background-color: ${(props) => props.theme.colors.primary};
+    background-color: ${(props) => props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto;
 `;
+
+
 
